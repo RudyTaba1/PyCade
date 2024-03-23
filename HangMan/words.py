a128e9d468e9e2e2f897bb8c1d7428fe0489d516
czine201 <94713530+czine201@users.noreply.github.com>
2024-03-23 12:25:00 -0500

    Create words.py

---
+++ HangMan/words.py
@@ -0,0 +1,9 @@
+words = {"abruptly", "absurd", "abyss", "affix", "askew", "beekeeper",
+         "blizzard", "boggle", "bookworm", "buxom", "buzzard", "buzzing", "cockiness",
+         "croquet", "crypt", "curacao", "cycle", "daiquiri", "dirndl", "disavow", "dizzying",
+         "duplex", "dwarves", "embezzle", "fuchsia", "funny", "gabby", "galaxy", "galvanize",
+         "gazebo", "giaour", "gizmo", "hyphen","icebox","injury", "ivory", "ivy","azzy",
+         "jelly", "jigsaw", "jinx", "jiujitsu", "jockey", "jogging", "joking", "jovial",
+         "joyful", "kiosk","kitsch", "kiwifruit", "klutz", "knapsack", "larynx", "lengths","lucky",
+         "luxury", "mystify", "strengths",
+         "stretch", "twelfths","unknown", "wristwatch", "youthful", "zodiac", "zombie"}
