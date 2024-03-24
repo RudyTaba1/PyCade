58b7d5f73c590fe4f89c58c0d559b65273f994c3
lwilson21 <awilson21@luc.edu>
2024-03-23 22:41:53 -0500

    colors and position adjustment

--- Pong/pong.py
+++ Pong/pong.py
@@ -4,13 +4,20 @@ import sys
 # State of project 3/22
 # window opens, paddles appear,
 # left paddle can be moved with w/s or up/down
+# stationary ball
 
 pg.init()
 
+# screen dimension constants
 WIDTH, HEIGHT = 800, 700
 CENTER_X, CENTER_Y = (WIDTH / 2), (HEIGHT / 2)
-screen = pg.display.set_mode((WIDTH, HEIGHT))
 
+#color constants
+WHITE = ((235, 235, 235))
+RED = ((186, 13, 13))
+BLUE = ((13, 42, 186))
+
+screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("pong")
 
 FPS = 30
@@ -36,7 +43,7 @@ attributes of paddle:
 
 
 class paddle:
-    def __init__(self, x, y, c=((255, 255, 255)), s=20, h=110, w=20):
+    def __init__(self, x, y, c=WHITE, s=20, h=110, w=20):
         self.color = c
         self.x_pos = x
         self.y_pos = y
@@ -44,16 +51,21 @@ class paddle:
         self.height = h
         self.width = w
 
+        # rectangle/drawing objects
         self.rect = pg.Rect(x, y, w, h)
         self.rect_draw = pg.draw.rect(screen, self.color, self.rect)
     
     def set_speed(self, s):
         self.speed = s
-
+    
+    def get_width(self):
+        return self.width
+    
     def display(self):
         self.rect_draw = pg.draw.rect(screen, self.color, self.rect)
 
     def update(self, y_mod):
+        # update speed
         self.y_pos += self.speed*y_mod
 
         # prevent from leaving bounds
@@ -62,10 +74,11 @@ class paddle:
         elif (self.y_pos + self.height >= HEIGHT):
             self.y_pos = HEIGHT-self.height
 
-
+        # update self rectangle
         self.rect = (self.x_pos, self.y_pos, self.width, self.height)
 
     def __str__(self):
+        # mostly for debugging, candidate for later deletion
         return ("paddle " + str(self.x_pos) + ", " + str(self.y_pos))
 
 """
@@ -77,34 +90,41 @@ Attributes of ball:
 """
 
 class ball:
-    def __init__(self, sz, sp, x, y, c):
+    def __init__(self, sz, sp, x, y, c = RED):
         self.size = sz
         self.speed = sp
         self.x_pos = x
         self.y_pos = y
         self.color = c
 
+        # rectangle/draw objects
         self.rect = pg.Rect(x, y, sz, sz)
         self.rect_draw = pg.draw.rect(screen, self.color, self.rect)
 
     def display(self):
         self.rect_draw = pg.draw.rect(screen, self.color, self.rect)
 
-player_paddle = paddle(10, 300)
-computer_paddle = paddle(770, 300)
-game_ball = ball(20, 20, (CENTER_X - 20), (CENTER_Y - 20), ((255, 255, 255)))
+# create game components
+player_paddle = paddle(10, CENTER_Y - 65)
+computer_paddle = paddle(WIDTH - 30, CENTER_Y - 65)
 
+game_ball = ball(20, 20, CENTER_X - 20, CENTER_Y - 20)
+
+# contains game logic
 def game_loop():
+
     running = True
+
     while(running):
         screen.fill((0, 0, 0))
 
-        #handle keypress
-        y_mod = 0
+        #handle quit
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
 
+        y_mod = 0
+        #handle keypress
         keys = pg.key.get_pressed()
         if keys[pg.K_UP] or keys[pg.K_w]:
             y_mod = -1
