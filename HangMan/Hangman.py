7550c35d2ab573fd1c142fc511357b85db0f81c1
czine201 <94713530+czine201@users.noreply.github.com>
2024-04-01 18:00:44 -0500

    Update Hangman.py

--- HangMan/Hangman.py
+++ HangMan/Hangman.py
@@ -1,52 +1,73 @@
-import random
-import string
-from words import words
 
-def get_valid_word(words):
-    word = random.choice(words) #assiging words into random choice to randomly choices word from list
-    while '-' in word or ' ' in word:
+#Hangman
+
+import random
+from words import words
+from hangman_visual import visual_dict
+import string
+
+
+def get_valid_word(words, min_length, max_length):
+    word = random.choice(words)  # randomly chooses something from the list
+    while len(word) < min_length or len(word) > max_length or "-" in word or ' ' in word:
         word = random.choice(words)
 
-    return word
+    return word.upper()
 
-def hangman():
-    word = get_valid_word(words)
-    #setting the letters in the word
-    word_letters = set(word)
-    #in alphabetical order
+def hangman_game(difficulty):
+    #Defining difficulty levels
+    difficulty_levels = {
+        "easy": {"word_length": (3, 6), "lives": 5},
+        "medium": {"word_length": (5, 8), "lives": 8},
+        "difficult": {"word_length": (7, 10), "lives": 10}
+    }
+    #select word based on difficulty level
+
+    min_length, max_length = difficulty_levels[difficulty]["word_length"]
+    word = get_valid_word(words, min_length, max_length)
+
+    #set lives based on difficulty level
+    lives = difficulty_levels[difficulty]["lives"]
+
+    word_letters = set(word) #letters in the word
     alphabet = set(string.ascii_uppercase)
     used_letters = set() #what the user has guessed
 
-    #getting the user input
-    while len(word_letters) > 0:
-        #letter used
-        #' '.join(['a', 'b', 'cd']) turns the string into ---> 'a b cd'
-        print('You have used these letters: ', ' '.join(used_letters))
-        #eliminating the used up letters and showcase the leftover letters to help
-        #the user guess the next letter in that character/word
+    # getting user input
+    while len(word_letters) > 0 and lives > 0:
+        # letters used
+        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
+
+        # what current word is (ie W - R D)
         word_list = [letter if letter in used_letters else '-' for letter in word]
+        print(visual_dict[lives]) #implementing hangman visual effects to show lives left
         print('Current word: ', ' '.join(word_list))
 
         user_letter = input('Guess a letter: ').upper()
-        #if it's a valid character that haven't used yet then add it into user_letter set
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
-        #remove the letter from word letter if the letter is in the word is already guessed
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+                print('')
+
+            else:
+                lives = lives - 1  # takes away a life if wrong
+                print('\nYour letter,', user_letter, 'is not in the word.')
 
         elif user_letter in used_letters:
-            print('You have already used that character. Please try again.')
+            print('\nYou have already used that letter. Guess another letter.')
+
         else:
-            print('Invalid character. Please try again.')
-            #using loop to make the user guess the word until they have guessed the actual word
+            print('\nThat is not a valid letter.')
 
-            #gets here when len(word_letter) > 0
+    # gets here when len(word_letters) == 0 OR when lives == 0
+    if lives == 0:
+        print(visual_dict[lives])
+        print('You died, sorry. The word was', word)
+    else:
+        print('YAY! You guessed the word', word, '!!')
 
-
-
-
-user_input = input('Type something:')
-#allow user to type in character that matches words from the list
-print(user_input)
-
+if __name__ == '__main__':
+    difficulty = input("Choose difficulty (easy/medium/difficult): ").lower()
+    hangman_game(difficulty)
