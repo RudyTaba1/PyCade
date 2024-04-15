02b91ca2b0a9a41df4f09427c7ffb71f425c1226
Rudy T <120225242+RudyTaba1@users.noreply.github.com>
2024-04-15 13:16:04 -0500

    established method for gmenu

--- MenuIntegration/Gmenu.py
+++ MenuIntegration/Gmenu.py
@@ -3,5 +3,21 @@ import HangMan as hm
 import Tetris as tet
 import Pong as p
 import tkinter as tk
+import system as os
 
+def logoScreen:
+    #It'll be an arcaddee like screen to start
+    #Getting Hangman to work since it is a cli tool
 
+def tetButton:
+    # button for tetris
+
+def pButton:
+    # button for pong
+
+def hmButton:
+    #button for hangman
+    #it will pull up a cli and run "python3 hangman.py"
+    #then play hangman
+
+def easterEgg:
